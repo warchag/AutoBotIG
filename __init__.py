5524c8e1397a79179fedec89aa7edecb6614fb2a
nara- <nara-@DESKTOP-F8VK5UN>
2020-04-10 10:36:33 +0700

    Update __init__.py
    done

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from bot import *
 if __name__ == "__main__":
     hashtags = input("=== input your tag ===\n")
     downloadcount = int(input("=== input your downloadcount ===\n"))
-    MYBOT = Instargram("warchag01","Billkyz131")
+    MYBOT = Instargram("user","password")
     MYBOT.Login()
     MYBOT.Search_Tag(hashtags)
     MYBOT.Like_photo(downloadcount,hashtags)
